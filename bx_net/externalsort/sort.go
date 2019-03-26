022b7d0701357426c0abf0120cf7e38f0a007c5c
lazyboy_chen7 <lazyboy_chen7@qq.com>
2019-03-26 21:23:41 +0800

    Merge sort simulate on multiple servers

---
+++ bx_net/externalsort/sort.go
@@ -0,0 +1,71 @@
+package main 
+
+import (
+	"os"
+	"fmt"
+	"bufio"
+	"strconv"
+	"bingxing/pipeline"
+)
+
+func main() {
+	pipeline.Init()
+	p := createNetworkPipeline("large.in", 80000000, 4)
+	writeTofile(p, "large.out")
+	printFile("large.out")
+}
+
+func writeTofile(p <-chan int, filename string) {
+	file,err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+
+	pipeline.WriterSink(writer, p)
+}
+
+func printFile(filename string) {
+	file,err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	
+	p := pipeline.ReaderSource(file, -1)
+	count := 0	
+	for v := range p {
+		fmt.Println(v)
+		count++
+		if count >= 100 {
+			break
+		}
+	}
+}
+
+func createNetworkPipeline(filename string, filesize, chunkcount int ) <-chan int {
+	chunkSize := filesize/chunkcount
+	pipeline.Init()
+	sortAddr := []string{}
+	for i:=0; i<chunkcount; i++ {
+		file,err := os.Open(filename)
+		if err != nil {
+			panic(err)
+		}
+		file.Seek(int64(i*chunkSize), 0)
+
+		source := pipeline.ReaderSource(bufio.NewReader(file), chunkSize)
+		addr := ":" + strconv.Itoa(7000 + i)
+		pipeline.NetworkSink(addr, pipeline.InMemSort(source))
+		sortAddr = append(sortAddr, addr)
+	}
+
+	sortResults := []<-chan int{}
+	for _,addr := range sortAddr {
+		sortResults = append(sortResults, pipeline.NetworkSource(addr))
+	}
+	return pipeline.MergeN(sortResults...)
+}
